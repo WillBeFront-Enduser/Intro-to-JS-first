9be8539420c8c0614d9608c282f160ff498b51db
WillBeFront-Enduser <pugbmobile40@gmail.com>
2022-11-30 16:51:48 +0300

    js first time

---
+++ script.js
@@ -0,0 +1,22 @@
+
+var nickname = prompt('Who are you?');
+alert(`My name is ${nickname}.`);
+console.log('My name is', nickname);
+document.write('My name is', nickname);
+var nickname2 = prompt('And what_s your name?')
+alert(`My name is ${nickname2}`);
+console.log('My name is', nickname2);
+document.write('And My name is', nickname2);
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
