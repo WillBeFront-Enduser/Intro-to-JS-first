81a48dc5066193f6ec9788c64f0fec08ede6897e
wassup <112810196+WillBeFront-Enduser@users.noreply.github.com>
2022-11-30 16:58:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 var nickname = prompt('Who are you?');
-alert(`My name is ${nickname}.`);
+alert(`My name is ${nickname} .`);
 console.log('My name is', nickname);
 document.write('My name is', nickname);
 var nickname2 = prompt('And what_s your name?')
-alert(`My name is ${nickname2}`);
+alert(`My name is ${nickname2} .`);
 console.log('My name is', nickname2);
 document.write('And My name is', nickname2);
 
