9ae9fb728bd1ca98470180f529bf0646357f695e
WillBeFront-Enduser <pugbmobile40@gmail.com>
2022-12-04 15:48:21 +0300

    mhm

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ var nickname = prompt('Who are you?');
 alert(`My name is ${nickname}.`);
 console.log('My name is', nickname);
 document.write('My name is', nickname);
-var nickname2 = prompt('And what_s your name?')
-alert(`My name is ${nickname2}`);
-console.log('My name is', nickname2);
-document.write('And My name is', nickname2);
+var nickname2 = prompt('And where are you from?')
+alert(`im from ${nickname2}`);
+console.log('im from', nickname2);
+document.write('im from', nickname2);
 
 
  
