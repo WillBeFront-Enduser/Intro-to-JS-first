0a58977724742a4daef03096e79125243cbceb57
WillBeFront-Enduser <pugbmobile40@gmail.com>
2022-12-06 19:41:29 +0300

    huh

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ var nickname = prompt('Who are you?');
 alert(`My name is ${nickname}.`);
 console.log('My name is', nickname);
 document.write('My name is', nickname);
-var nickname2 = prompt('And where are you from?')
-alert(`Im from ${nickname2}`);
-console.log('Im from', nickname2);
-document.write('Im from', nickname2);
